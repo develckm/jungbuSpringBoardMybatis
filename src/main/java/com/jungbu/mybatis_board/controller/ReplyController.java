fb506901884b4a186b37b6be95188cad1c344888
최경민 <develckm@gmail.com>
2022-10-25 12:50:11 +0900

    reply crud

--- src/main/java/com/jungbu/mybatis_board/controller/ReplyController.java
+++ src/main/java/com/jungbu/mybatis_board/controller/ReplyController.java
@@ -1,14 +1,20 @@
 package com.jungbu.mybatis_board.controller;
 
+import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,14 +36,16 @@ public class ReplyController {
 			@SessionAttribute UserDto loginUser
 			) {
 		reply.setUserId(loginUser.getUserId());
-		String contentTypes[]=img.getContentType().split("/"); // image/jpeg, application/json
 		int insert=0;
 		try {
-			if(img!=null&&!img.isEmpty()&&contentTypes[0].equals("image")) {
-				String fileName="reply_"+System.currentTimeMillis()+"_"+((int)(Math.random()*10000))+"."+contentTypes[1];
-				Path path=Paths.get(imgPath+"/"+fileName);
-				img.transferTo(path);	
-				reply.setImgPath(fileName);
+			if(img!=null&&!img.isEmpty()) {
+				String contentTypes[]=img.getContentType().split("/"); // image/jpeg, application/json
+				if(contentTypes[0].equals("image")) {
+					String fileName="reply_"+System.currentTimeMillis()+"_"+((int)(Math.random()*10000))+"."+contentTypes[1];
+					Path path=Paths.get(imgPath+"/"+fileName);
+					img.transferTo(path);	
+					reply.setImgPath(fileName);					
+				}
 			}
 			insert=replyMapper.insert(reply);
 			System.out.println(insert);
@@ -46,4 +54,77 @@ public class ReplyController {
 		}
 		return "redirect:/board/detail.do?boardNo="+reply.getBoardNo();
 	}
+	@GetMapping("/update.do")
+	public void update(
+			@RequestParam(required=true) int replyNo,
+			Model model) {
+		ReplyDto reply=null;
+		try {
+			reply=replyMapper.detail(replyNo);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		model.addAttribute("reply", reply);
+	}	
+	
+	
+	@GetMapping("/delete.do")
+	public String delete(
+			@RequestParam(required=true) int replyNo,
+			@SessionAttribute UserDto loginUser,
+			HttpSession session
+			) {
+		ReplyDto reply=null;
+		int delete=0;
+		try {
+			reply=replyMapper.detail(replyNo);
+			if(reply.getUserId().equals(loginUser.getUserId())) {
+				delete=replyMapper.delete(replyNo);
+				if(delete>0&&reply.getImgPath()!=null) {
+					File imgFile=new File(imgPath+"/"+reply.getImgPath());
+					System.out.println("이미지 파일 삭제:"+imgFile.delete());
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String msg=(delete>0)?"삭제 성공":"삭제 실패";
+		session.setAttribute("msg", msg);
+		return "redirect:/board/detail.do?boardNo="+reply.getBoardNo();
+	}
+	@PostMapping("/update.do")
+	public String update(
+			ReplyDto reply,
+			@SessionAttribute UserDto loginUser,
+			HttpSession session,
+			MultipartFile img
+			) {
+		int update=0;
+		try {
+			if(img!=null&& !img.isEmpty()) {
+				String[]contensTypes=img.getContentType().split("/");
+				if(contensTypes[0].equals("image")) {
+					String fileName="reply_"+System.currentTimeMillis()+"_"+((int)(Math.random()*10000))+"."+contensTypes[1];
+					Path path=Paths.get(imgPath+"/"+fileName);
+					img.transferTo(path);	
+					if(reply.getImgPath()!=null) {
+						File imgFile=new File(imgPath+"/"+reply.getImgPath());
+						System.out.println("이미지 파일 삭제:"+imgFile.delete());
+					}
+					reply.setImgPath(fileName);					
+				}
+			}
+			update=replyMapper.update(reply);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		String msg=(update>0)?"수정 성공":"수정 실패";
+		session.setAttribute("msg", msg);
+		return "redirect:/board/detail.do?boardNo="+reply.getBoardNo();
+
+	}
 }
+
+
+
