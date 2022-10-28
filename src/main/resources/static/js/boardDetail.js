c34e46be7fb1bdb947c3d668bef6ce874c0c0a36
최경민 <develckm@gmail.com>
2022-10-28 12:34:41 +0900

    reply reply insert

--- src/main/resources/static/js/boardDetail.js
+++ src/main/resources/static/js/boardDetail.js
@@ -2,28 +2,32 @@ const replyInserForm=document.forms["replyInserForm"];
 const replyList=document.getElementById("replyList");
 
 if(replyInserForm!=null){
-	replyInserForm.addEventListener("submit",async(e)=>{
-		e.preventDefault();
-		let boardNo=replyInserForm.boardNo.value;
-		let url=replyInserForm.action;
-		let data=new FormData(replyInserForm); 
-		//multipart/form-data일대 파라미터를 blob으로 처리
-		let resp=await fetch(url,{body:data,method:"post"});
-		if(resp.status==200){//로그인이 안되어 있을때
-			let json=await resp.json();
-			let msg="";
-			switch(json.status){
-				case 0: msg="등록실패(db에러)"; break;
-				case 1: 
-					msg="등록성공"; 
-					readReplyList(boardNo);
-					break;
-				case -1: msg="로그인을 하세요."; break;
-			}
-			alert(msg);
-		}
-	});
+	replyInserForm.addEventListener("submit",insertReply);
 }
+async function insertReply(e){
+	e.preventDefault();
+	const replyInserForm=e.target
+	let boardNo=replyInserForm.boardNo.value;
+	let url=replyInserForm.action;
+	let data=new FormData(replyInserForm); 
+	//multipart/form-data일대 파라미터를 blob으로 처리
+	let resp=await fetch(url,{body:data,method:"post"});
+	if(resp.status==200){//로그인이 안되어 있을때
+		let json=await resp.json();
+		let msg="";
+		switch(json.status){
+			case 0: msg="등록실패(db에러)"; break;
+			case 1: 
+				msg="등록성공"; 
+				readReplyList(boardNo);
+				break;
+			case -1: msg="로그인을 하세요."; break;
+		}
+		alert(msg);
+	}
+}
+
+
 async function readReplyList(boardNo,page=1){
 	let url="/reply/"+boardNo+"/list.do?page="+page;
 	let resp=await fetch(url);
