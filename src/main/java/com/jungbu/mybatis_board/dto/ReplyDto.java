a7dbc712cb72acf88eaa04f76086a458d24ba92a
최경민 <develckm@gmail.com>
2022-10-28 11:48:24 +0900

    reply reply join read

--- src/main/java/com/jungbu/mybatis_board/dto/ReplyDto.java
+++ src/main/java/com/jungbu/mybatis_board/dto/ReplyDto.java
@@ -1,20 +1,22 @@
 package com.jungbu.mybatis_board.dto;
 
 import java.util.Date;
+import java.util.List;
 
 import lombok.Data;
 /*
-+-----------+--------------+------+-----+-------------------+-------------------+
-| Field     | Type         | Null | Key | Default           | Extra             |
-+-----------+--------------+------+-----+-------------------+-------------------+
-| reply_no  | int          | NO   | PRI | NULL              | auto_increment    |
-| title     | varchar(255) | NO   |     | NULL              |                   |
-| contents  | varchar(255) | YES  |     |                   |                   |
-| post_time | datetime     | YES  |     | CURRENT_TIMESTAMP | DEFAULT_GENERATED |
-| img_path  | varchar(255) | YES  |     | NULL              |                   |
-| board_no  | int          | NO   | MUL | NULL              |                   |
-| user_id   | varchar(255) | NO   | MUL | NULL              |                   |
-+-----------+--------------+------+-----+-------------------+-------------------+*/
++-------------+--------------+------+-----+-------------------+-------------------+
+| Field       | Type         | Null | Key | Default           | Extra             |
++-------------+--------------+------+-----+-------------------+-------------------+
+| reply_no    | int          | NO   | PRI | NULL              | auto_increment    |
+| title       | varchar(255) | NO   |     | NULL              |                   |
+| contents    | varchar(255) | YES  |     |                   |                   |
+| post_time   | datetime     | YES  |     | CURRENT_TIMESTAMP | DEFAULT_GENERATED |
+| img_path    | varchar(255) | YES  |     | NULL              |                   |
+| board_no    | int          | NO   | MUL | NULL              |                   |
+| user_id     | varchar(255) | NO   | MUL | NULL              |                   |
+| fk_reply_no | int          | YES  | MUL | NULL              |                   |
++-------------+--------------+------+-----+-------------------+-------------------+*/
 @Data
 public class ReplyDto {
 	private int replyNo; //PK 
@@ -26,7 +28,9 @@ public class ReplyDto {
 	private String userId;//FK: user.user_id
 	private int likes;//select count(*) from reply_prefer weher prefer=1 AND board_no=no     
 	private int bads; //select count(*) from reply_prefer weher prefer=0 AND board_no=no     
-
+	
+	private Integer fkReplyNo; //FK : self join(reply.reply_no is null)
+	private List<ReplyDto> replyList; //reply : reply = 1 : N   대 댓글
 }
 
 
